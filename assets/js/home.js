4b7e59b2282040eae2fcda7ce94ec5a1cd329e0a
Hoang Huyen <1851061774@e.tlu.edu.vn>
2021-10-22 12:03:42 +0700

    code giao dien menu login right

---
+++ assets/js/home.js
@@ -0,0 +1,33 @@
+/* xử lý sự kiện bật tắt volume */
+$(document).ready(function(){
+    $('.fa-volume-up').click(function(event){
+        $('.fa-volume-up').hide();
+        $('.fa-volume-mute').show();
+    });
+    $('.fa-volume-mute').click(function(event){
+        $('.fa-volume-up').show();
+        $('.fa-volume-mute').hide();
+    });
+});
+
+/* xử lý sự kiện bật tắt xoay */
+$(document).ready(function(){
+    $('.rotate').click(function(event){
+        $('.rotate').hide();
+        $('.lock').show();
+    });
+    $('.lock').click(function(event){
+        $('.rotate').show();
+        $('.lock').hide();
+    });
+});
+
+/* xử lý sự kiện show/hide menu login */
+$(document).ready(function(){
+    $('#menu-login .sub-item').hide();
+    $('.menu-login-right').click(function(event){
+        $('#menu-login .sub-item').toggle();
+        
+    });
+   
+});
